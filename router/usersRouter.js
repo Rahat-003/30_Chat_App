acf0ca917a248e03c49de33d2a8f532d3c3c7ce9
PC3-Kadritech <kadritechtest@gmail.com>
2024-04-01 17:21:01 +0600

    LWS #32 - 27:51

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,14 +1,19 @@
 // external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 // internal imports
 const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const addUserValidators = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
-// login page
-router.get("/",decorateHtmlResponse("Users"), getUsers);
+// users page
+router.get("/", decorateHtmlResponse("Users "), getUsers);
+
+// add user
+router.post("/", avatarUpload, addUserValidators);
 
 module.exports = router;
